3bd749044acec7310d117e97687c7ad904fa840e
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-08-20 15:58:00 +0400

    cleanup old configs
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,11 +1,43 @@
 #!/bin/bash -ex
 
+yum -y upgrade  
+cat <<EOF > /etc/resolv.conf
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+options timeout:2 attempts:1 rotate
+EOF
 
-cat /etc/centos-release
-grep -q " 6\." /etc/centos-release && rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm && yum -y install cloud-init
+cat <<EOF >  /etc/sysconfig/network-scripts/ifcfg-eth0
+DEVICE="eth0"
+BOOTPROTO=dhcp
+NM_CONTROLLED="no"
+PERSISTENT_DHCLIENT=1
+ONBOOT="yes"
+TYPE=Ethernet
+DEFROUTE=yes
+PEERDNS=yes
+PEERROUTES=yes
+IPV4_FAILURE_FATAL=no
+IPV6INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_PEERDNS=yes
+IPV6_PEERROUTES=yes
+IPV6_FAILURE_FATAL=yes
+NAME="eth0"
+EOF
+
 grep -q " 7\." /etc/centos-release && rpm -Uvh http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm && yum -y --enablerepo="epel" install cloud-init
 
-
-rm -f /etc/udev/rules.d/70-persistent-net.rules 
-
 yum -y install qemu-guest-agent
+sed -i 's|^disable_root:.*|disable_root: 0|g' /etc/cloud/cloud.cfg
+sed -i 's|^ssh_pwauth:.*|ssh_pwauth: 1|g' /etc/cloud/cloud.cfg
+ln --symbolic /dev/null /etc/udev/rules.d/80-net-name-slot.rules
+sed -i 's|#UseDNS yes|UseDNS no|g' /etc/ssh/sshd_config
+sed -i 's|GSSAPIAuthentication yes|GSSAPIAuthentication no|g'  /etc/ssh/sshd_config
+sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
+sed -i 's|#PermitRootLogin yes|PermitRootLogin yes|g' /etc/ssh/sshd_config
+
+cat /etc/cloud/cloud.cfg
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/sh -ex
 
 echo "==> Cleaning up yum cache of metadata and packages to save space"
 yum -y clean all
 
 rm -rf /tmp/*
 
+rm -f /etc/udev/rules.d/70-persistent-net.rules
